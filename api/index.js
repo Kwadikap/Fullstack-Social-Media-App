bfb5ee155d0b65864962288f4dd9b040eed3d37f
Kwadwo <kwadikap@gmail.com>
2022-03-02 12:26:08 -0500

    complete front-end and back-end

---
+++ api/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
+const multer = require('multer');
+const path = require('path');
+const cors = require('cors');
+
+// app.use(cors({
+//     origin: '127.0.0.1:3000',
+//     credentials: true,
+// }));
+
+
+dotenv.config(); 
+
+mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true}, () => {
+    console.log('Connected to database ✅')
+});
+
+app.use('/images', express.static(path.join(__dirname, 'public/images')))
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb) => {
+        cb(null, 'public/images')
+    },
+    filename: (req,file,cb) => {
+        cb(null, req.body.name)
+    }
+})
+
+const upload = multer({storage});
+app.post('/api/upload', upload.single('file'), (req, res) => {
+    try {
+        return res.status(200).json('File uploaded successfully.');
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/posts', postRoute);
+
+
+
+app.listen(3001, () => {
+    console.log("Backend server is running...");
+}) 
---
+++ react-front-end/src/App.js
@@ -0,0 +1,26 @@
+import { useContext } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import Homepage from './pages/homepage/Homepage'
+import Login from './pages/login/Login'
+import ProfilePage from './pages/profilePage/ProfilePage'
+import Register from './pages/register/Register'
+import { AuthContext } from './context/AuthContext';
+
+
+function App() {
+
+  const {user} = useContext(AuthContext);
+  return (
+      <>
+        <Routes>
+            <Route path='/' element={ user ? <Homepage /> : <Register /> } />
+            <Route path='/login' element={ user ? <Navigate to='/' /> : <Login />} />
+            <Route path='/register' element={ user ? <Navigate to='/' /> : <Register />} />
+            <Route path='/profile/:username' element={<ProfilePage />} />
+        </Routes>
+      </>
+  );
+}
+
+export default App;
